904937909d39b73a066d8e8e82defc5315316ba8
ROCKET-P13 <135278016+ROCKET-P13@users.noreply.github.com>
2025-08-22 07:46:56 -0400

    fix: infrastructure fix

--- ASPNETLambdaAPIInfrastructure/bin/aspnet_lambda_api_infrastructure.ts
+++ ASPNETLambdaAPIInfrastructure/bin/aspnet_lambda_api_infrastructure.ts
@@ -1,87 +1,20 @@
+#!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as rds from 'aws-cdk-lib/aws-rds';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
-import * as apigw from 'aws-cdk-lib/aws-apigatewayv2';
-import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
-import * as path from 'path';
+import { LambdaApiInfrastructureStack } from '../lib/aspnet_lambda_api_infrastructure-stack';
 
-export class LambdaApiInfrastructureStack extends cdk.Stack {
-	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-		super(scope, id, props);
+const app = new cdk.App();
+new LambdaApiInfrastructureStack(app, 'LambdaApiInfrastructureStack', {
+  /* If you don't specify 'env', this stack will be environment-agnostic.
+   * Account/Region-dependent features and context lookups will not work,
+   * but a single synthesized template can be deployed anywhere. */
 
-		const vpc = new ec2.Vpc(this, 'ServerlessVpc', {
-			maxAzs: 2,
-			subnetConfiguration: [
-				{ name: 'Public',  subnetType: ec2.SubnetType.PUBLIC,              cidrMask: 24 },
-				{ name: 'Private', subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS, cidrMask: 24 },
-			],
-		});
+  /* Uncomment the next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration. */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-		const dbSecurityGroup = new ec2.SecurityGroup(this, 'DbSecurityGroup', { vpc, allowAllOutbound: true });
-		const lambdaSecurityGroup = new ec2.SecurityGroup(this, 'LambdaSecurityGroup', { vpc, allowAllOutbound: true });
-		dbSecurityGroup.addIngressRule(lambdaSecurityGroup, ec2.Port.tcp(5432), 'Allow Lambda to reach Postgres');
+  /* Uncomment the next line if you know exactly what Account and Region you
+   * want to deploy the stack to. */
+  // env: { account: '123456789012', region: 'us-east-1' },
 
-		const dbSecret = new secretsmanager.Secret(this, 'DbSecret', {
-			secretName: 'ASPNETLambdaAPI_Secret',
-			generateSecretString: {
-				secretStringTemplate: JSON.stringify({ username: 'aspnetlambdaapi' }),
-				generateStringKey: 'password',
-				excludeCharacters: `;/@": %$><'`,
-				passwordLength: 16,
-			},
-		});
-
-		const dbInstance = new rds.DatabaseInstance(this, 'PostgresDb', {
-			engine: rds.DatabaseInstanceEngine.postgres({
-				version: rds.PostgresEngineVersion.VER_17_2,
-			}),
-			vpc,
-			credentials: rds.Credentials.fromSecret(dbSecret),
-			vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
-			securityGroups: [dbSecurityGroup],
-			allocatedStorage: 20,
-			maxAllocatedStorage: 100,
-			publiclyAccessible: false,
-			databaseName: 'books',
-			removalPolicy: cdk.RemovalPolicy.DESTROY,
-		});
-
-		const apiLambda = new lambda.Function(this, 'ApiLambda', {
-			runtime: lambda.Runtime.DOTNET_8,
-			handler: 'ASPNETLambdaAPI::ASPNETLambdaAPI.LambdaEntryPoint::FunctionHandlerAsync',
-			code: lambda.Code.fromAsset(
-				path.join(__dirname, '../../ASPNETLambdaAPI/bin/Release/net8.0/publish')
-			),
-			vpc,
-			timeout: cdk.Duration.seconds(30),
-			memorySize: 512,
-			securityGroups: [lambdaSecurityGroup],
-			vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
-			environment: {
-				POSTGRES_SECRET_ARN: dbSecret.secretArn,
-				DB_HOST: dbInstance.dbInstanceEndpointAddress,
-				DB_PORT: dbInstance.dbInstanceEndpointPort,
-				DB_NAME: 'books',
-			},
-		});
-
-		dbSecret.grantRead(apiLambda);
-
-		const httpApi = new apigw.HttpApi(this, 'ApiGateway', {
-			apiName: 'ASPNETLambdaAPI',
-			description: 'HTTP API for ASP.NET Core on Lambda',
-		});
-
-		httpApi.addRoutes({
-			path: '/{proxy+}',
-			methods: [apigw.HttpMethod.ANY],
-			integration: new integrations.HttpLambdaIntegration('ApiLambdaIntegration', apiLambda),
-		});
-
-		new cdk.CfnOutput(this, 'ApiUrl', { value: httpApi.url! });
-		new cdk.CfnOutput(this, 'DbEndpoint', { value: dbInstance.dbInstanceEndpointAddress });
-	}
-}
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
